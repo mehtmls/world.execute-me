8a911b4233807e220396c73d757d60241aa0f0c9
淘汰郎 <121741836+mehtmls@users.noreply.github.com>
2026-02-19 21:54:23 +0800

    更新 demo.js

--- demo.js
+++ demo.js
@@ -16,88 +16,109 @@ me.data = new Uint8Array([0, 97, 115, 109, 1, 0, 0, 0])
 you.data = me.data
 //Set up our new world
 world = new Response(you.data, {
-    headers: { 'Content-Type': 'application/wasm' }
+    headers: {
+        'Content-Type': 'application/wasm'
+    }
 });
 //And let's begin the simulation
-WebAssembly.instantiateStreaming(world).then(()=>{
-    let me = {...window.me}
-    let you = {...window.you}
+WebAssembly.instantiateStreaming(world).then((simulation)=> {
+    let me = {
+        ...window.me
+    }
+    let you = {
+        ...window.you
+    }
     //If I'm a set of point
     if (me.like = {
-        x:0,y:0
-    }){
+        x: 0, y: 0
+    }) {
         // Then I will give you my dimension
         you.dimension = `(${me.like.x},${me.like.y})`
-        me.like=null
+        me.like = null
     };
     //If I'm a circle
     if (me.like = {
-        posX : 0,
-        posY : 0,
-        r : 1
+        posX: 0,
+        posY: 0,
+        r: 1
     }) {
         //Then I will give you my circumference
         you.circumference = 2*me.like.r*Math.PI
-        me.like=null
+        me.like = null
     };
     //If I'm a sine wave
     if (me.like = Math.sin) {
         //Then you can sit on all my tangents
         you.posY = me.like(Math.PI/2)
-        me.like=null
+        me.like = null
     }
     //If I approach infinity
-    if (me.value = Infinity){
+    if (me.value = Infinity) {
         //Then you can be my limitations
-        you.limit = Math.min(me.value,Number.MAX_SAFE_INTEGER)
+        you.limit = Math.min(me.value, Number.MAX_SAFE_INTEGER)
     }
     //Switch my current
     me.current=!me.current
     //To AC
-    me.current='AC'
+    me.current = 'AC'
     //to DC
-    me.current='DC'
+    me.current = 'DC'
     //And then blind my vision
     Object.defineProperty(me, 'vision', {
-      enumerable: true,
-      get:()=>{return void 0}
+        enumerable: true,
+        get: ()=> {
+            return void 0
+        }
     });
     //So dizzy, so dizzy
     for (let i = 0; i < 10000; i++) {
         me.value = Math.random()
     }
     //Oh, we can travel. To AD
-    me.travel='AD'
+    me.travel = 'AD'
     //to BC
-    me.travel='BC'
+    me.travel = 'BC'
     //And we can unite
-    we={
+    we = {
         me,
         you,
     }
     //So deeply, so deeply
-    we.we=we
+    we.we = we
     //If I can, if I can
-    if (me.can = true) if (me.can = true){
+    if (me.can = true) if (me.can = true) {
         //Give you all the simulations
-        you = {...me,...you}
+        you = {
+            ...me,
+            ...you
+        }
         //Then I can, then I can Be your only satisfaction
         if (me.can) if (me.can) {
             you.satisfaction = me
 
             Object.defineProperty(you, 'satisfaction', {
-             value: you.satisfaction,
-             writable: false,
-             enumerable: true
+                value: you.satisfaction,
+                writable: false,
+                enumerable: true
             });
         }
     }
     //If I can make you happy
-    if (you.happy = me.can){
+    if (you.happy = me.can) {
         //I will run the execution
-        (()=>{
+        (()=> {
+            window.sustain = 0
             //Though we are trapped. In this strange, strange simulation
-            return window.execution
+            world.execute = function() {
+                if (sustain++ < 8) {
+                    console.log("%cEXECUTED SUCCESS", "color:green")
+                } else {
+                    console.log("%cEXECUTED SUCCESS", "color:red")
+                    console.error(Error("god can't try selfkill"))
+                }
+            }
+            return simulation.strange = "very strange",
+            simulation
         })()
 
     }
@@ -114,15 +135,25 @@ WebAssembly.instantiateStreaming(world).then(()=>{
         me.antioxidants = null
     }
     //If I'm a tabby cat
-    if (me.like = {//猫 娘 (确信)
+    if (me.like = {
+        //猫 (me.gender=="F"?'娘':'郎') (确信)
         type: 'cat',
-        skin:'tabby'
+        skin: 'tabby'
     }) {
         //Then I will purr for your enjoyment
         you.enjoyment = me.purr?.()
     }
     //If I'm the only God
-    if (me.isGOD = true) {
+    Object.defineProperty(me, 'isGOD', {
+        enumerable: true,
+        get: ()=> {
+            return true
+        },
+        set: ()=> {
+            console.error(TypeError('ILLEGAL ARGUMENTS:GOD always is true'))
+        }
+    });
+    if (me.isGOD) {
         //Then you're the proof of my existence
         you.proof = `god is ${me.isGOD}ly`
     }
@@ -132,24 +163,76 @@ WebAssembly.instantiateStreaming(world).then(()=>{
     me.gender = 'M'
     //And then do whatever From AM to PM
     time = 'AM'
-    do{
-       whatever=function(){
-           //(now me is man)
-           (me.fuck = function(who){
-               return who.child = ['F']
-               
-           })(you)
-       }
-       whatever()
-       time = (Math.random()*2>>0)?'AM':'PM'
-    } while (time!=='PM');
+    do {
+        whatever = function() {
+            //(now me is man)
+            (me.fuck = function() {
+                return you.child = ['F']
+
+            })(you)
+        }
+        whatever()
+        time = (Math.random()*2>>0)?'AM': 'PM'
+    } while (time !== 'PM');
     //Oh, my switch role To S
     me.role = 'S'
     //to M
     me.role = 'M'
     //So we can enter The trance, the trance
-    (we.trance=()=>{
-        me.at = you.at = 'entirely'
-    })()
-
+    me.feeling = you.felling = 'trance'
+    //If I can If I can
+    if (me.can) if (me.can) {
+        //这里还是true
+        //feel your VIBRATIONS
+        me.feeling = you.VIBRATIONS = me.can
+        //Then I can Then I can
+        if (me.can) if (me.can) {
+            new Promise((resolve, reject)=> {
+                me.feeling?resolve("VIBRATIONING"): reject('me is blind')
+                //finally be COMPLETION
+            }).finally(()=> {
+                me.COMPLETION = true
+            })
+        }
+    }
+    //Though you have left
+    you = null
+    //You have left*4
+    you = null
+    you = null
+    you = null
+    you = null
+    //You have left me in ISOLATION
+    you = null
+    me.feeling = "ISOLATION"
+    me.can = you
+    //If I can If I can
+    if (me.can) if (me.can) {
+        //erase all the pointless FRAGMENTS
+        me.mem = new Uint8Array(10).fill(0)
+        //Then maybe Then maybe you won't leave me so DISHEARTENED
+        me.feeling = !"DISHEARTENED"
+    }
+    //Challenging your god
+    //You have made some ILLEGAL ARGUMENTS
+    me.isGOD = false
+    //EXECUTION*12
+    world.execute()
+    world.execute()
+    world.execute()
+    world.execute()
+    world.execute()
+    world.execute()
+    world.execute()
+    world.execute()
+    world.execute()
+    world.execute()
+    world.execute()
+    world.execute()
+    for (let i = 0; i < 6; i++) {
+        //EIN DOS TROIS NE FEM LIU
+    }
+    //EXECUTION
+    world.execute()
+    //TO be COUNTIUN
 })
